2e8283e50f3a108754141b54b81fde662e8938e9
Rombus <MrRombus@yandex.ru>
2025-07-26 21:04:22 +0300

    fillCheck

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.style.scss'
-import { array, clamp } from '@utils/std'
+import { array, clamp, Point } from '@utils/std'
 import { useState } from '@utils/useState'
 import { Stage, Layer, Rect } from 'react-konva'
 import { cloneElement, useRef } from 'react'
 import { RectConfig } from 'konva/lib/shapes/Rect'
 import { KonvaEventObject, Node, NodeConfig } from 'konva/lib/Node'
 
-const GRID_SIZE = 20
+const GRID_SIZE = 9
 const CELL_SIZE = 30
 
 const cells = array(GRID_SIZE).map((row) => (
@@ -25,9 +25,16 @@ const cells = array(GRID_SIZE).map((row) => (
     ))
 ))
 
+interface LineResult {
+    x: number
+    y: number
+    fill: ("hor" | "ver" | "dialL" | "dialR")[]
+}
+
 export function App() {
     const grid = useState(cells)
     const lastCell = useRef<number[]>([])
+    const gridData = useRef<Point[]>([])
 
     const updateSpecificCell = (row: number, col: number, fill: string) => {
         grid.set((v) => {
@@ -59,12 +66,60 @@ export function App() {
         updateSpecificCell(row, col, 'yellow')
     }
 
+    const fillOnClick = (e: KonvaEventObject<MouseEvent, Node<NodeConfig>>) => {
+        //grid.set([])
+        const pointer = e.target.getRelativePointerPosition()
+        if (!pointer) return
+
+        const row = Math.floor(clamp(pointer.y - 1, 0) / CELL_SIZE)
+        const col = Math.floor(clamp(pointer.x - 1, 0) / CELL_SIZE)
+        
+        if (row < 0 || row >= GRID_SIZE) return
+        if (col < 0 || col >= GRID_SIZE) return
+
+        gridData.current.push(new Point(col, row))
+        console.log(gridData.current)
+        console.log(checkTree())
+        updateSpecificCell(row, col, 'yellow')
+    }
+
+    const checkTree = (): LineResult | null => {
+        let result: LineResult | null = null
+
+        gridData.current.forEach((point) => {
+            const nearHor = gridData.current.filter((v) => {
+                return v.y == point.y && (v.x == point.x + 1 || v.x == point.x - 1)
+            })
+            const nearVer = gridData.current.filter((v) => {
+                return v.x == point.x && (v.y == point.y + 1 || v.y == point.y - 1)
+            })
+            const nearDialL = gridData.current.filter((v) => {
+                return (v.x == point.x - 1 && v.y == point.y - 1) || (v.x == point.x + 1 && v.y == point.y + 1)
+            })
+            const nearDialR = gridData.current.filter((v) => {
+                return (v.x == point.x + 1 && v.y == point.y - 1) || (v.x == point.x - 1 && v.y == point.y + 1)
+            })
+
+            const fill: LineResult["fill"] = []
+            if (nearHor.length == 2) fill.push("hor")
+            if (nearVer.length == 2) fill.push("ver")
+            if (nearDialL.length == 2) fill.push("dialL")
+            if (nearDialR.length == 2) fill.push("dialR")
+
+            if (fill.length != 0) {
+                result = {x: point.x, y: point.y, fill}
+            }
+        })
+        return result
+    }
+
     return (
         <div className="container">
             <Stage
                 width={window.innerWidth}
                 height={window.innerHeight}
-                onMouseMove={layerMouseMove}
+                //onMouseMove={layerMouseMove}
+                onClick={fillOnClick}
             >
                 <Layer>
                     {grid.value}
--- frontend/src/utils/std.ts
+++ frontend/src/utils/std.ts
@@ -5,3 +5,13 @@ export function array(length: number) {
 export function clamp(value: number, min: number, max?: number) {
     return Math.max(min, max != null ? Math.min(value, max) : value)
 }
+
+export class Point {
+    x: number
+    y: number
+
+    constructor(x: number, y: number) {
+        this.x = x
+        this.y = y
+    }
+}
